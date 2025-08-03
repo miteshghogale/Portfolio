ee6b9d02e8605c3c96902002c96c545679b38f89
mitesh <mitesh100ghogale@gmail.com>
2025-08-03 13:47:18 +0530

    about started

--- React_Portfolio/src/App.jsx
+++ React_Portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./App.css";
 import Header from './Componets/Header/Header';
 import Home from './Componets/Home/Home';
+import About from './Componets/About/About';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     <Header/>
     <main className='main'>
       <Home/>
+      <About/>
     </main>
     </>
   )
---
+++ React_Portfolio/src/Componets/About/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./about.css"
+import AboutImg from "../../assets/about.jpg"
+import CV from "../../assets/John-Cv.pdf"
+import Info from './Info'
+
+const About = () => {
+  return (
+    <section className="about section" id="about">
+        <h2 className="section_title">About me</h2>
+        <span className="section_subtitle">My Introduction</span>
+
+        <div className="about_container container grid">
+            <img src={AboutImg} alt="" className="about_img" />
+
+            <div className="about_data">
+                <Info/>
+                <p className="about_description">Frontend developer, I create web pages with UI / UX user interface, I have years of experience and many clients are happy with the projects carried out.</p>
+
+                <a download="" href={CV} className="button button--flex">Download CV <i class="uil uil-import"></i></a>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
